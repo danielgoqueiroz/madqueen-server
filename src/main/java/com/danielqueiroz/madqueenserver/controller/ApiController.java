ece5b9d7e1270f794847af28701f333a5b0a7783
Daniel Gomes Queiroz <contato@danielqueiroz.com>
2021-03-30 20:28:08 -0300

    Cors

--- src/main/java/com/danielqueiroz/madqueenserver/controller/ApiController.java
+++ src/main/java/com/danielqueiroz/madqueenserver/controller/ApiController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/login")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ApiController {
 	
