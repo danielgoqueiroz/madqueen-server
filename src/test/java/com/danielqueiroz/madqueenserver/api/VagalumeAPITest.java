4a0aa5840cfb54c9880f3e03f8892ba4872b09ed
Daniel Gomes Queiroz <contato@danielqueiroz.com>
2021-04-11 16:44:25 -0300

    mudança de lib json

--- src/test/java/com/danielqueiroz/madqueenserver/api/VagalumeAPITest.java
+++ src/test/java/com/danielqueiroz/madqueenserver/api/VagalumeAPITest.java
@@ -5,9 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.danielqueiroz.madqueenserver.api.model.MusicDTO;
